77e7695d88faeca6196da1190afc59aa9ce904d3
hkloudou <hkloudou@gmail.com>
2019-02-23 17:58:30 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var uglify = require('gulp-uglify');
+var pipeline = require('readable-stream').pipeline;
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", function () {
+gulp.task('build', function () {
     return tsProject.src()
         .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
+        .js.pipe(gulp.dest('dist'));
 });
+
+gulp.task('compress', function () {
+    return pipeline(
+          gulp.src('dist/*.js'),
+          uglify(),
+          gulp.dest('dist')
+    );
+});
+
+gulp.task('default', gulp.series('build'));
