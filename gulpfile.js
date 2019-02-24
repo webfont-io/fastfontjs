ca0ed48be9ff658dd27ef6a45a2137713dcc06a8
hkloudou <hkloudou@gmail.com>
2019-02-24 17:07:54 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require("gulp");
 var gutil = require("gulp-util");
 var webpack = require("webpack");
 var config = require("./webpack.config.js");
+var npm = require("./package.json");
 var browserSync = require('browser-sync').create();
+var fs = require('fs');
 
 // 静态服务器
 gulp.task('browser-sync', function() {
@@ -15,8 +17,11 @@ gulp.task('browser-sync', function() {
     gulp.watch('./dist/**/*.js').on('change', browserSync.reload);
 });
 
+gulp.task("reversion",function(){
+    fs.writeFileSync("./src/version.ts","export default '" + npm.version + "'");
+})
+
 gulp.task("webpack",function(){
-    //config.plugins = config.optimization.minimizer;
     return new Promise((resolve,reject) => {
         webpack(config, function(err,stats) {
             if (err) throw new gutil.PluginError("webpack",err);
@@ -46,4 +51,4 @@ gulp.task("webpack",function(){
         })
     })
 });
-gulp.task('default', gulp.series('webpack','browser-sync'));
+gulp.task('default', gulp.series('reversion','webpack','browser-sync'));
